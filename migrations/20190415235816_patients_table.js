2b0465caeb404db74b0e47edd825156387257f06
cubsfan46350 <cubsfan.46350@msn.com>
2019-04-16 00:27:35 -0500

    created patients table with knex

---
+++ migrations/20190415235816_patients_table.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('patients_table', tbl => {
+        tbl.increments();
+        tbl.string('username')
+            .notNullable()
+            .unique('username');
+        tbl.timestamp('createdAt').defaultTo(knex.fn.now());
+
+        tbl.string('password')
+            .notNullable();
+        tbl.string('firstName')
+            .notNullable();
+        tbl.string('lastName')
+            .notNullable();
+        tbl.string('email')
+            .notNullable();
+        tbl.string('role')
+            .notNullable();
+        
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('patients_table');
+};
