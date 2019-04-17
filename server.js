b59f0358a641df22f0593e153b63705597ffe77f
cubsfan46350 <cubsfan.46350@msn.com>
2019-04-17 13:49:15 -0500

    created server template and added cors as dependency

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const server = express();
-
+const knex = require('knex');
+const dBConfig = require('./knexfile.js');
+const db = knex(dBConfig.development);
+const cors = require('cors');
 //Middleware
 server.use(express.json());
 
