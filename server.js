bc7d37dfe40c7de5341192976d16c8fe7413fde1
cubsfan46350 <cubsfan.46350@msn.com>
2019-04-17 21:35:13 -0500

    created post endpoint

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const db = knex(dBConfig.development);
 const cors = require('cors');
 //Middleware
 server.use(express.json());
+server.use(cors());
+//Endpoints
+server.post('/api/addUser', async (req, res) => {
+    try {
+        const user = await db('patients_table').insert(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error Adding User',
+        });
+    }
+});
 
 
 
