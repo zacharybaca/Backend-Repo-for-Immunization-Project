f1ce31ae65b7e210eead4ae73e595727984a837e
cubsfan46350 <cubsfan.46350@msn.com>
2019-04-19 00:28:00 -0500

    created login endpoint with jsonwebtoken and bcrypt

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const db = knex(dBConfig.development);
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const Users = require('./database/users-model.js');
+const jwt = require('jsonwebtoken');
 //Middleware
 server.use(express.json());
 server.use(cors());
@@ -24,6 +25,40 @@ server.post('/api/addUser',  (req, res) => {
             res.status(500).json(error);
         });
 });
+
+server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user);
+                res.status(200).json({
+                    message: 'Welcome ${user}!',
+                    token
+                });
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username
+    };
+    const secret = require('./secrets.js').jwtSecret;
+    const options = {
+        expiresIn: '1h'
+    }
+
+    return jwt.sign(payload, secret, options);
+}
 
 
      
